a8b299c03e53ffe150bf881d34d954400fafed54
Prince0206 <124934940+Prince0206@users.noreply.github.com>
2024-07-01 15:37:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Arcade-Game
+ Play the Greatest RPG ever to be made! Soon to be played on consule
---
+++ js/Gamemanager.js
@@ -0,0 +1,70 @@
+let GameManager = {
+    setGameStart: function(classType) {
+        this.resetPlayer(classType);
+        this.setPreFight();
+    },
+    resetPlayer: function(classType) {
+        switch (classType) {
+            case "Warrior":
+                player = new Player(classType, 200, 0, 200, 100, 50);
+                break;
+            case "Rogue":
+                player = new Player(classType, 100, 0, 100, 150, 200);
+                break;
+            case "Mage":
+                player = new Player(classType, 80, 0, 50, 200, 50);
+                break;
+            case "Hunter":
+                player = new Player(classType, 200, 0, 50, 200, 100);
+                break;
+        }
+        let getInterface = document.querySelector(".interface");
+        getInterface.innerHTML = '<img src="images/Heroes/' +
+        classType.toLowerCase() + '.jpeg" class="images-Heroes"><div><h3>' +classType
+         + '</h3><p class="health-player">Health: ' + player.health + '</p><p>Mana: ' + player.mana + 
+         '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility 
+         + '</p><p>Speed: ' + player.speed + '</p></div>';
+        
+    },
+    setPreFight: function() {
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getArena = document.querySelector(".arena");
+        getHeader.innerHTML = '<p>Task: Find an enemy!</P>';
+        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Search for enemy!</a>';
+        getArena.style.visibility = "visible";
+    },
+    setFight: function() {
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getEnemy = document.querySelector(".enemy");
+        // Create enemy!
+        let enemy00 = new Enemy( "Killer", 200, 0, 150, 100 , 100);
+        let enemy01 = new Enemy( "Rapist", 100, 0, 50, 90, 100);
+        let enemy02 = new Enemy( "Thief", 120, 0, 60, 100, 50);
+        let enemy03 = new Enemy( "psycho", 150, 0,150, 140, 100);
+        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(4));
+       switch (chooseRandomEnemy) {
+        case 0:
+            enemy = enemy00;
+            break;
+        case 1:
+            enemy = enemy01;
+            break;
+        case 2:
+            enemy = enemy02;
+            break;
+        case 3:
+            enemy = enemy03;
+            break;
+       }
+       getHeader.innerHTML = '<p>Task: Choose your move!';
+       getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
+       getEnemy.innerHTML = '<img src= " images/enemy/' + 
+       enemy.enemyType.toLowerCase() + '.jpeg" alt="' + enemy.enemyType + '" class="images-enemy"><div><h3>' + enemy.enemyType
+       + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + 
+       '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility
+       + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+    }
+    
+}
